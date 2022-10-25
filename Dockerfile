674584cdec813ab75a84a8332a4ee52f478dd8ac
lostsquirrel <lostsquirreli@hotmail.com>
2022-10-25 15:52:20 +0800

    add the scripts

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM docker.io/python:3.10 AS builder
+
+RUN pip install --user pipenv
+
+ENV PIPENV_VENV_IN_PROJECT=1
+
+ADD Pipfile.lock Pipfile /usr/src/
+
+WORKDIR /usr/src
+
+RUN /root/.local/bin/pipenv sync
+
+FROM docker.io/python:3.10 AS runtime
+
+RUN mkdir -v /usr/src/venv
+
+COPY --from=builder /usr/src/.venv/ /usr/src/venv/
+
+RUN adduser --uid 1000 musicforprogramming
+
+WORKDIR /usr/src/
+
+USER musicforprogramming
+
+CMD ["./venv/bin/python", "-m", "musicforprogramming.py"]
---
+++ musicforprogramming.py
@@ -0,0 +1,63 @@
+import os
+import feedparser
+from pathlib import Path
+import logging
+import requests
+
+feed_url = "https://musicforprogramming.net/rss.xml"
+
+etag_file = "musicforprogramming.etag"
+
+logger = logging.getLogger(__name__)
+
+
+def get_etag():
+
+    p = Path(data_path) / etag_file
+    if not p.exists():
+        return None
+    return p.read().decode()
+
+
+def save_etag(etag: str):
+
+    p = Path(data_path) / etag_file
+    if not p.exists():
+        p.touch()
+    p.write_text(etag)
+
+
+def get_filename(full_link: str):
+    return full_link.split("music_for_programming_")[1]
+
+
+def download():
+    d = feedparser.parse(feed_url, get_etag())
+    # print(d.keys())
+    # print(d.etag)
+    # feed = d.feed
+    # print(type(feed))
+    # print(feed.keys())
+    for item in d.entries:
+        file_url = item.id
+        f = Path(data_path) / get_filename(file_url)
+        if not f.exists():
+            with requests.get(file_url, stream=True) as resp:
+                if resp.status_code != 200:
+                    logger.error(resp.content)
+                else:
+                    logger.info(f"start to download {f}")
+                    totalbits = 0
+                    with f.open("wb") as fh:
+                        for chunk in resp.iter_content(chunk_size=1024):
+                            if chunk:
+                                totalbits += 1024
+                                fh.write(chunk)
+                    logger.info(f"{f} download finished in {totalbits}")
+    save_etag(d.etag)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    data_path = os.getenv('DATA_PATH', "/data/music")
+    download()
